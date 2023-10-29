4218426632ef876f5f854cfd197a9370cb8fedcb
Ladecepcion <64137025+Charam12@users.noreply.github.com>
2023-10-29 20:01:15 +0700

    Update cloudinary in app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dms1okjpd/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
